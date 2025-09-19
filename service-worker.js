ef198b84335b407e973e9b53b30a54f96aac2447
Rostyslav Tsyhanov <150455334+r1shek@users.noreply.github.com>
2025-09-19 07:28:19 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('pwa-v1').then(c=>c.addAll([
-    './','./index.html','./manifest.json','./service-worker.js','./prompts.json','./icons/apple-touch-icon-180.png'
-  ])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
